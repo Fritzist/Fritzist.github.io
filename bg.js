c0a8d843eda1a41de6795740d92cad91b90b6073
Fritzist <danielhartl856@gmail.com>
2022-04-11 22:21:32 +0200

    cleaner

---
+++ bg.js
@@ -0,0 +1,22 @@
+
+const backgrounds = [
+    "bg0.jpg",
+    "bg1.jpg",
+    "bg2.jpg",
+    "bg3.jpg",
+    "bg4.jpg",
+    "bg5.jpg",
+    "bg6.jpg",
+    "bg7.jpg",
+    "bg8.jpg",
+    "bg9.jpg",
+    "bg10.jpeg",
+    "bg11.jpg",
+    "bg12.jpg",
+    "bg13.jpg",
+    "bg14.jpg",
+];
+let bgnum = (parseInt(localStorage.bgnum?localStorage.bgnum:0) + 1)%backgrounds.length;
+localStorage.bgnum = bgnum;
+let background = backgrounds[bgnum];
+document.querySelector("body").style = `background-image: url('bg/${background}')`;
